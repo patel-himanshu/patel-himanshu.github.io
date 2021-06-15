6dd1b94c5236b27ee753c6f5accf432f1a49dca0
Himanshu Patel <44549418+patel-himanshu@users.noreply.github.com>
2021-06-16 02:40:11 +0530

    feat: Add Sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,199 @@
+import { useState } from "react";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import { Link, NavLink } from "react-router-dom";
+import clsx from "clsx";
+
+import Drawer from "@material-ui/core/Drawer";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import List from "@material-ui/core/List";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import IconButton from "@material-ui/core/IconButton";
+
+import KeyboardArrowLeftIcon from "@material-ui/icons/KeyboardArrowLeft";
+import KeyboardArrowRightIcon from "@material-ui/icons/KeyboardArrowRight";
+import MenuIcon from "@material-ui/icons/Menu";
+
+import palette from "../styling/palette";
+
+const drawerWidth = 180;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: 36,
+  },
+  hide: {
+    display: "none",
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: "nowrap",
+  },
+  drawerOpen: {
+    width: drawerWidth,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerClose: {
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: "hidden",
+    width: theme.spacing(8) + 1,
+  },
+  toolbar: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    padding: theme.spacing(0, 1),
+    ...theme.mixins.toolbar,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+  socials: {
+    marginTop: "auto",
+    marginBottom: "0.5rem",
+  },
+}));
+
+const Sidebar = (props) => {
+  const classes = useStyles();
+  const theme = useTheme();
+
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const handleDrawerOpen = () => {
+    setDrawerOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setDrawerOpen(false);
+  };
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: drawerOpen,
+        })}
+      >
+        <Toolbar style={{ padding: "0 1rem" }}>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, {
+              [classes.hide]: drawerOpen,
+            })}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            <Link to="/" style={{ color: "white", textDecoration: "none" }}>
+              Himanshu Patel
+            </Link>
+          </Typography>
+
+          <div className={classes.grow} />
+        </Toolbar>
+      </AppBar>
+
+      <Drawer
+        variant="permanent"
+        className={clsx(classes.drawer, {
+          [classes.drawerOpen]: drawerOpen,
+          [classes.drawerClose]: !drawerOpen,
+        })}
+        classes={{
+          paper: clsx({
+            [classes.drawerOpen]: drawerOpen,
+            [classes.drawerClose]: !drawerOpen,
+          }),
+        }}
+      >
+        <div className={classes.toolbar}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === "rtl" ? (
+              <KeyboardArrowRightIcon />
+            ) : (
+              <KeyboardArrowLeftIcon />
+            )}
+          </IconButton>
+          <strong>Welcome</strong>
+        </div>
+        <Divider />
+        <List>
+          {props.sidebarOptions.map(({ label, icon, path }) => (
+            <NavLink
+              key={label}
+              to={path}
+              style={{ color: "black", textDecoration: "none" }}
+              activeStyle={{ color: palette.primary2 }}
+            >
+              <ListItem button>
+                <ListItemIcon style={{ minWidth: "3rem" }}>{icon}</ListItemIcon>
+                <ListItemText primary={label} />
+              </ListItem>
+            </NavLink>
+          ))}
+        </List>
+        <Divider />
+        <List className={classes.socials}>
+          {props.socialMediaHandles.map(({ label, icon, path }) => (
+            <a
+              key={label}
+              href={path}
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: "black", textDecoration: "none" }}
+            >
+              <ListItem button>
+                <ListItemIcon style={{ minWidth: "3rem" }}>{icon}</ListItemIcon>
+                <ListItemText primary={label} />
+              </ListItem>
+            </a>
+          ))}
+        </List>
+      </Drawer>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        {props.children}
+      </main>
+    </div>
+  );
+};
+
+export default Sidebar;
