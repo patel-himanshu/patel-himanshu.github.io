0d742ee194e6aa10c2007bdcc58c22d08cc2e03c
Himanshu Patel <44549418+patel-himanshu@users.noreply.github.com>
2021-06-19 17:54:09 +0530

    chore: Change CustomText to CustomHeader

---
+++ src/components/CustomHeader.jsx
@@ -0,0 +1,29 @@
+import { Typography, withStyles } from "@material-ui/core";
+import { fontSizes, fontWeights } from "../styling/typography";
+
+const CustomHeader = (props) => {
+  const { size, weight, children, ...rest } = props;
+
+  const StyledText = withStyles((theme) => ({
+    root: {
+      color: "white",
+      backgroundColor: "black",
+      fontSize: fontSizes[size] || "1.5rem",
+      fontWeight: fontWeights[weight] || weight || "normal",
+    },
+  }))(Typography);
+
+  return (
+    <StyledText
+      {...rest}
+      style={{
+        padding: "0.25rem 1rem",
+        marginBottom: "1.5rem",
+      }}
+    >
+      <span>{children}</span>
+    </StyledText>
+  );
+};
+
+export default CustomHeader;
