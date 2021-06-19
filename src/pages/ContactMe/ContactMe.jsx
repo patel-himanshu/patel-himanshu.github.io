a1d86cfd2ec6cbcefa67cbd497b5742e8a05ae66
Himanshu Patel <44549418+patel-himanshu@users.noreply.github.com>
2021-06-19 17:55:54 +0530

    feat: Complete ContactMe page

--- src/pages/ContactMe/ContactMe.jsx
+++ src/pages/ContactMe/ContactMe.jsx
@@ -1,7 +1,63 @@
 import "./ContactMe.css";
+import { TextareaAutosize } from "@material-ui/core";
+import SendIcon from "@material-ui/icons/Send";
+import CustomButton from "../../components/CustomButton";
 
 const ContactMe = () => {
-  return <section>This is Contact Me section GitHub LinkedIn Twitter</section>;
+  return (
+    <article>
+      <form method="POST" action="https://formspree.io/f/mbjqyqlz">
+        <fieldset>
+          <legend>Contact Me</legend>
+
+          <p>
+            Please feel free to contact me regarding any questions, feedback or
+            suggestions that you may have.
+          </p>
+
+          <label for="name">
+            Name <span style={{ color: "red" }}>*</span>
+          </label>
+          <input type="text" id="name" name="name" required />
+
+          <label for="email">
+            E-Mail <span style={{ color: "red" }}>*</span>
+          </label>
+          <input type="email" id="email" name="email" required />
+
+          <label for="subject">
+            Subject <span style={{ color: "red" }}>*</span>
+          </label>
+          <input type="text" id="subject" name="subject" required />
+
+          <label for="message">
+            Message <span style={{ color: "red" }}>*</span>
+          </label>
+          {/* <textarea type="text" id="message" name="message" required /> */}
+          <TextareaAutosize
+            type="text"
+            id="message"
+            name="message"
+            rowsMin="6"
+            required
+          />
+
+          <div align="center">
+            <CustomButton
+              type="submit"
+              style={{
+                margin: "1rem 0",
+                letterSpacing: "1px",
+              }}
+              endIcon={<SendIcon />}
+            >
+              Send E-Mail
+            </CustomButton>
+          </div>
+        </fieldset>
+      </form>
+    </article>
+  );
 };
 
 export default ContactMe;
