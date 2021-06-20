247fc78d01b8bdaa1451a7528f25d3ac2f233f29
Himanshu Patel <44549418+patel-himanshu@users.noreply.github.com>
2021-06-20 23:47:38 +0530

    feat: Add basic info and skills sub-section

--- src/pages/AboutMe/AboutMe.jsx
+++ src/pages/AboutMe/AboutMe.jsx
@@ -1,5 +1,64 @@
+import "./AboutMe.css";
+import Skills from "./Skills";
+import coding from "../../assets/coding.png";
+
+import Grid from "@material-ui/core/Grid";
+
 const AboutMe = () => {
-  return <>This is the About Me page.</>;
+  return (
+    <article className="page-content">
+      {/* "About Me" section */}
+      <section className="page-section">
+        <header>
+          <span className="section-header">About Me</span>
+        </header>
+        <Grid container>
+          <Grid item md={5}>
+            <p>
+              My name is Himanshu Patel. Currently, I am
+              <ul>
+                <li>
+                  👨🏼‍🎓 a final-year CSE undergrad at IIIT Naya Raipur (India)
+                </li>
+                <li>💼 working on getting a job/internship</li>
+                <li>💻 primarily interested in full-stack development</li>
+                <li>🧰 learning React and Django</li>
+                <li>👩‍💻 trying to become more involved in open source</li>
+              </ul>
+              I enjoy writing code, especially in Python and JavaScript.
+              <br />I am always eager to learn and explore new things.
+            </p>
+          </Grid>
+          <Grid item md={5}>
+            <img
+              src={coding}
+              alt="A person coding on laptop"
+              style={{ marginLeft: "auto", height: "16rem" }}
+            />
+          </Grid>
+        </Grid>
+      </section>
+
+      <br />
+      {/* "My Skills" section */}
+      <section className="page-section">
+        <header>
+          <span className="section-header">My Skills</span>
+        </header>
+        <p>
+          <Skills />
+        </p>
+      </section>
+
+      <br />
+      {/* "Education" section */}
+      <section className="page-section">
+        <header>
+          <span className="section-header">Education</span>
+        </header>
+      </section>
+    </article>
+  );
 };
 
 export default AboutMe;
---
+++ src/pages/AboutMe/Skills.jsx
@@ -0,0 +1,130 @@
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+
+import angular from "../../assets/logos/angular.png";
+import bootstrap from "../../assets/logos/bootstrap.png";
+import css from "../../assets/logos/css.png";
+import django from "../../assets/logos/django.png";
+// import express from "../../assets/logos/express.png";
+import html from "../../assets/logos/html.png";
+import git from "../../assets/logos/git.png";
+import javascript from "../../assets/logos/javascript.png";
+// import latex from "../../assets/logos/latex.png";
+import linux from "../../assets/logos/linux.png";
+import materialui from "../../assets/logos/material-ui.png";
+import mongodb from "../../assets/logos/mongodb.png";
+import mysql from "../../assets/logos/mysql.png";
+// import nodejs from "../../assets/logos/nodejs.png";
+// import postgresql from "../../assets/logos/postgresql.png";
+import postman from "../../assets/logos/postman.png";
+import python from "../../assets/logos/python.png";
+import react from "../../assets/logos/react.png";
+import redux from "../../assets/logos/redux.png";
+import sqlite from "../../assets/logos/sqlite.png";
+// import tensorflow from "../../assets/logos/tensorflow.png";
+import typescript from "../../assets/logos/typescript.png";
+import windows from "../../assets/logos/windows.png";
+
+const data = [
+  {
+    category: "Languages",
+    skills: [
+      { name: "Python", icon: python },
+      { name: "JavaScript", icon: javascript },
+      { name: "TypeScript", icon: typescript },
+      { name: "HTML", icon: html },
+      { name: "CSS", icon: css },
+    ],
+  },
+  {
+    category: "Frameworks & Libraries",
+    skills: [
+      { name: "React", icon: react },
+      { name: "Django", icon: django },
+      { name: "Bootstrap", icon: bootstrap },
+      { name: "Material-UI", icon: materialui },
+      { name: "Angular", icon: angular },
+      //   { name: "Express", icon: express },
+      //   { name: "TensorFlow", icon: tensorflow },
+    ],
+  },
+  {
+    category: "Databases",
+    skills: [
+      { name: "MySQL", icon: mysql },
+      { name: "SQLite", icon: sqlite },
+      { name: "MongoDB", icon: mongodb },
+      //   { name: "PostgreSQL", icon: postgresql },
+    ],
+  },
+  {
+    category: "Tools & Platforms",
+    skills: [
+      { name: "Git", icon: git },
+      { name: "Redux", icon: redux },
+      //   { name: "Node.js", icon: nodejs },
+      { name: "Postman", icon: postman },
+      //   { name: "LATEX", icon: latex },
+      { name: "Windows", icon: windows },
+      { name: "Linux", icon: linux },
+    ],
+  },
+];
+
+const Skills = () => {
+  return (
+    <Grid container>
+      {data.map((elem) => {
+        return (
+          <Grid item md={3} sm={6} xs={12}>
+            <Card
+              key={elem.category}
+              variant="outlined"
+              style={{ backgroundColor: "#eaeaea", margin: "0.5rem" }}
+            >
+              <CardContent>
+                <strong>{elem.category}</strong>
+                <hr />
+                <div>
+                  <List style={{ padding: "0" }}>
+                    {elem.skills.map((skill) => {
+                      return (
+                        <ListItem disableGutters="true">
+                          <ListItemIcon
+                            style={{ width: "100px", marginRight: "1rem" }}
+                          >
+                            <img
+                              src={skill.icon}
+                              alt={skill.name}
+                              style={{
+                                filter: "drop-shadow(0 0 0.5px #59170D)",
+                                display: "block",
+                                margin: "auto",
+                                height: "40px",
+                              }}
+                            />
+                          </ListItemIcon>
+                          <ListItemText
+                            primary={skill.name}
+                            // secondary={secondary ? 'Secondary text' : null}
+                          />
+                        </ListItem>
+                      );
+                    })}
+                  </List>
+                </div>
+              </CardContent>
+            </Card>
+          </Grid>
+        );
+      })}
+    </Grid>
+  );
+};
+
+export default Skills;
