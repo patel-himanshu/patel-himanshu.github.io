0ca0e35093ec98af33c0efa560ffa1781c503645
Himanshu Patel <44549418+patel-himanshu@users.noreply.github.com>
2021-07-04 03:22:24 +0530

    feat: Add Experience section

--- src/pages/Experience/Experience.jsx
+++ src/pages/Experience/Experience.jsx
@@ -1,5 +1,114 @@
+import { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Accordion from "@material-ui/core/Accordion";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import Typography from "@material-ui/core/Typography";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import "./Experience.css";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    padding: "0 1rem",
+  },
+  heading: {
+    flexBasis: "33.33%",
+    flexShrink: 0,
+  },
+  secondaryHeading: {
+    color: theme.palette.text.secondary,
+  },
+}));
+
+const internshipDetails = [
+  {
+    label: "label1",
+    company: "Cartel101",
+    role: "Software Developer Intern",
+    details: [
+      "Work from Home (April 2021 - May 2021)",
+      "Worked on the front end of an Asset Performance Manager (APM) project, using React and Material-UI",
+      "Performed multiple API integrations on the front end, and used Redux for state management",
+    ],
+  },
+  {
+    label: "label2",
+    company: "ChaloPadhe",
+    role: "Software Developer Intern",
+    details: [
+      "Work from Home (August 2020 - October 2020)",
+      "Helped in the creation of multiple APIs using Django REST Framework",
+      "Helped in developing the front end of the website for the organization using Angular",
+    ],
+  },
+];
+
 const Experience = () => {
-  return <>This is the Experience page.</>;
+  const classes = useStyles();
+  const [expanded, setExpanded] = useState(false);
+
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
+  return (
+    <>
+      <section className="page-section">
+        <header>
+          <span className="section-header">Experience</span>
+        </header>
+
+        {/* Internship section section */}
+        <section>
+          <h2 className="subsection-header">Internships</h2>
+          <div className="underline" />
+          <div className={classes.root}>
+            {internshipDetails.map((internship) => {
+              return (
+                <Accordion
+                  expanded={expanded === `${internship.label}`}
+                  onChange={handleChange(`${internship.label}`)}
+                >
+                  <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                    <Typography className={classes.heading}>
+                      {internship.role}
+                    </Typography>
+                    <Typography
+                      className={classes.secondaryHeading}
+                      style={{ margin: "auto" }}
+                    >
+                      <strong>{internship.company}</strong>
+                    </Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <ul className="accordian-details">
+                      {internship.details.map((detail) => {
+                        return <li>{detail}</li>;
+                      })}
+                    </ul>
+                  </AccordionDetails>
+                </Accordion>
+              );
+            })}
+          </div>
+        </section>
+
+        {/* Positions of Responsibility section */}
+        <section>
+          <h2 className="subsection-header">Positions of Responsibility</h2>
+          <div className="underline" />
+          <ul className="positions-responsibility">
+            <li>Core Member of The Society of Coders, IIIT-Naya Raipur</li>
+            <li>
+              Website Manager of college’s website for Student Activity Center
+              and IEEE Student Branch
+            </li>
+          </ul>
+        </section>
+      </section>
+    </>
+  );
 };
 
 export default Experience;
