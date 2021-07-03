923a98875c2f97983a3eba254539a6e9efbb1ecf
Himanshu Patel <44549418+patel-himanshu@users.noreply.github.com>
2021-07-03 22:11:22 +0530

    feat: Complete Blogs page

--- src/pages/Blogs/Blogs.jsx
+++ src/pages/Blogs/Blogs.jsx
@@ -1,5 +1,22 @@
+import { Link } from "react-router-dom";
+
 const Blogs = () => {
-  return <>This is the Blogs page.</>;
+  return (
+    <article className="page-content">
+      {/* "About Me" section */}
+      <section className="page-section">
+        <header>
+          <span className="section-header">Blogs</span>
+        </header>
+        <div style={{ textAlign: "center" }}>
+          Sorry, I'm currently working on this section. Return after a few days
+          to see what's new.
+          <br />
+          Return to <Link to="/">Homepage</Link>.
+        </div>
+      </section>
+    </article>
+  );
 };
 
 export default Blogs;
