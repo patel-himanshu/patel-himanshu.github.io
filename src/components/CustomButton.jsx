23b2ff6c5c1f5e5a4f6eeeec938c9f788d2bd585
Himanshu Patel <44549418+patel-himanshu@users.noreply.github.com>
2021-07-08 22:59:05 +0530

    style: Add more customizability for custom buttons

--- src/components/CustomButton.jsx
+++ src/components/CustomButton.jsx
@@ -3,24 +3,34 @@ import { fontSizes, fontWeights } from "../styling/typography";
 import palette from "../styling/palette";
 
 const CustomButton = (props) => {
-  const { children, padding, size, weight, ...rest } = props;
+  const {
+    children,
+    padding,
+    size,
+    weight,
+    border,
+    bgColor1,
+    bgColor2,
+    textColor,
+    ...rest
+  } = props;
 
   const StyledButton = withStyles((theme) => ({
     root: {
-      backgroundColor: palette.primary1,
+      backgroundColor: bgColor1 || palette.primary1,
       borderRadius: "3px",
-      border: 0,
+      border: border || 0,
       outline: "none",
       padding: padding || "8px 32px",
       textTransform: "none",
       transition: "0.3s",
       "&:hover": {
-        backgroundColor: palette.primary0,
+        backgroundColor: bgColor2 || palette.primary0,
         // boxShadow: `0 0 15px ${palette.primary3}`,
       },
     },
     label: {
-      color: "white",
+      color: textColor || "white",
       fontSize: fontSizes[size] || "1.25rem",
       fontWeight: fontWeights[weight] || weight || "normal",
     },
