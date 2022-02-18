61f1358c965f9662f3c40bea871d88f17e9e107f
Himanshu Patel <44549418+patel-himanshu@users.noreply.github.com>
2022-02-18 20:50:09 +0530

    style: Update design of PoR section

--- src/pages/Experience/Experience.jsx
+++ src/pages/Experience/Experience.jsx
@@ -8,142 +8,154 @@ import Typography from "@material-ui/core/Typography";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 import {
-  internshipDetails,
-  positionsOfResponsibility,
+	internshipDetails,
+	positionsOfResponsibility,
 } from "./experienceDetails";
 import "./Experience.css";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    width: "100%",
-    // padding: "0 0.25rem",
-  },
-  heading: {
-    flexBasis: "33.33%",
-    flexShrink: 0,
-    [theme.breakpoints.down("sm")]: {
-      fontSize: "1rem",
-    },
-  },
-  secondaryHeading: {
-    color: theme.palette.text.secondary,
-    [theme.breakpoints.down("sm")]: {
-      fontSize: "0.9rem",
-    },
-  },
-  details: {
-    marginTop: "0.5rem",
-    [theme.breakpoints.down("sm")]: {
-      fontSize: "0.9rem",
-    },
-  },
-  positions: {
-    [theme.breakpoints.down("sm")]: {
-      fontSize: "0.9rem",
-    },
-  },
+	root: {
+		width: "100%",
+		// padding: "0 0.25rem",
+	},
+	heading: {
+		flexBasis: "33.33%",
+		flexShrink: 0,
+		[theme.breakpoints.down("sm")]: {
+			fontSize: "1rem",
+		},
+	},
+	secondaryHeading: {
+		color: theme.palette.text.secondary,
+		[theme.breakpoints.down("sm")]: {
+			fontSize: "0.9rem",
+		},
+	},
+	details: {
+		marginTop: "0.5rem",
+		[theme.breakpoints.down("sm")]: {
+			fontSize: "0.9rem",
+		},
+	},
+	positions: {
+		[theme.breakpoints.down("sm")]: {
+			fontSize: "0.9rem",
+		},
+	},
 }));
 
 const Experience = () => {
-  const classes = useStyles();
-  const [expanded, setExpanded] = useState(false);
+	const classes = useStyles();
+	const [expanded, setExpanded] = useState(false);
 
-  const handleChange = (panel) => (event, isExpanded) => {
-    setExpanded(isExpanded ? panel : false);
-  };
+	const handleChange = (panel) => (event, isExpanded) => {
+		setExpanded(isExpanded ? panel : false);
+	};
 
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, []);
 
-  return (
-    <article className="page-content">
-      <section className="page-section">
-        <header>
-          <span className="section-header">Experience</span>
-        </header>
+	return (
+		<article className="page-content">
+			<section className="page-section">
+				<header>
+					<span className="section-header">Experience</span>
+				</header>
 
-        {/* Internship section section */}
-        <section>
-          <h2 className="subsection-header">Internships</h2>
-          <div className="underline" />
-          <div className={classes.root}>
-            {internshipDetails.map((internship) => {
-              return (
-                <Accordion
-                  key={internship.label}
-                  expanded={expanded === `${internship.label}`}
-                  onChange={handleChange(`${internship.label}`)}
-                >
-                  <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                    <Grid container>
-                      <Grid item lg={2} md={3} xs={12}>
-                        <Typography
-                          className={classes.heading}
-                          style={{ margin: "auto" }}
-                        >
-                          <strong>{internship.company}</strong>
-                        </Typography>
-                      </Grid>
-                      <Grid item md={4} xs={12}>
-                        <Typography
-                          className={classes.heading}
-                          style={{ margin: "auto" }}
-                        >
-                          {internship.role}
-                        </Typography>
-                      </Grid>
-                      <Grid item>
-                        <Typography
-                          className={classes.secondaryHeading}
-                          style={{ margin: "auto" }}
-                        >
-                          ({internship.duration})
-                        </Typography>
-                      </Grid>
-                    </Grid>
-                  </AccordionSummary>
-                  <AccordionDetails>
-                    <ul className="accordian-details">
-                      {internship.details.map((detail) => {
-                        return (
-                          <li key={detail} className={classes.details}>
-                            {detail}
-                          </li>
-                        );
-                      })}
-                    </ul>
-                  </AccordionDetails>
-                </Accordion>
-              );
-            })}
-          </div>
-        </section>
+				{/* Internship section section */}
+				<section>
+					<h2 className="subsection-header">Internships</h2>
+					<div className="underline" />
+					<div className={classes.root}>
+						{internshipDetails.map((internship) => {
+							return (
+								<Accordion
+									key={internship.label}
+									expanded={expanded === `${internship.label}`}
+									onChange={handleChange(`${internship.label}`)}
+								>
+									<AccordionSummary expandIcon={<ExpandMoreIcon />}>
+										<Grid container>
+											<Grid item md={4} xs={12}>
+												<Typography
+													className={classes.heading}
+													style={{ margin: "auto" }}
+												>
+													<strong>{internship.company}</strong>
+												</Typography>
+											</Grid>
+											<Grid item md={4} xs={12}>
+												<Typography
+													className={classes.heading}
+													style={{ margin: "auto" }}
+												>
+													{internship.role}
+												</Typography>
+											</Grid>
+											<Grid item>
+												<Typography
+													className={classes.secondaryHeading}
+													style={{ margin: "auto" }}
+												>
+													({internship.duration})
+												</Typography>
+											</Grid>
+										</Grid>
+									</AccordionSummary>
+									<AccordionDetails>
+										<ul className="accordian-details">
+											{internship.details.map((detail) => {
+												return (
+													<li key={detail} className={classes.details}>
+														{detail}
+													</li>
+												);
+											})}
+										</ul>
+									</AccordionDetails>
+								</Accordion>
+							);
+						})}
+					</div>
+				</section>
 
-        {/* Positions of Responsibility section */}
-        <section>
-          <h2 className="subsection-header">Positions of Responsibility</h2>
-          <div className="underline" />
-          <div className={classes.root}>
-            {positionsOfResponsibility.map((position) => {
-              return (
-                <Accordion key={position}>
-                  <Grid container style={{ padding: "1rem" }}>
-                    <Typography
-                      variant="subtitle2"
-                      className={classes.positions}
-                    >
-                      {position}
-                    </Typography>
-                  </Grid>
-                </Accordion>
-              );
-            })}
-          </div>
-        </section>
-      </section>
-    </article>
-  );
+				{/* Positions of Responsibility section */}
+				<section>
+					<h2 className="subsection-header">Positions of Responsibility</h2>
+					<div className="underline" />
+					<div className={classes.root}>
+						{positionsOfResponsibility.map((position) => {
+							return (
+								<Accordion key={position}>
+									<Grid
+										container
+										style={{
+											padding: "0.5rem 1rem 0 1rem",
+										}}
+									>
+										<Grid item md={8} xs={12}>
+											<Typography
+												variant="subtitle1"
+												className={classes.positions}
+											>
+												{position.description}
+											</Typography>
+										</Grid>
+										<Grid item>
+											<Typography className={classes.secondaryHeading}>
+												{position.duration}
+											</Typography>
+										</Grid>
+									</Grid>
+								</Accordion>
+							);
+						})}
+					</div>
+				</section>
+			</section>
+		</article>
+	);
 };
 
 export default memo(Experience);
--- src/pages/Experience/experienceDetails.js
+++ src/pages/Experience/experienceDetails.js
@@ -1,36 +1,43 @@
 export const internshipDetails = [
-  {
-    label: "label3",
-    company: "Carl Zeiss India (Bangalore)",
-    role: "Full-Stack Developer Intern",
-    duration: "January 2022 - Present",
-    details: [],
-  },
-  {
-    label: "label2",
-    company: "Cartel101",
-    role: "Software Developer Intern",
-    duration: "April 2021 - May 2021",
-    details: [
-      "Worked on the front end of an Asset Performance Manager (APM) project",
-      "Performed multiple API integrations on the front end and used Redux for state management",
-      "Used React, Redux, HTML, CSS, Material-UI, Postman, Git, Figma",
-    ],
-  },
-  {
-    label: "label1",
-    company: "ChaloPadhe",
-    role: "Software Developer Intern",
-    duration: "August 2020 - October 2020",
-    details: [
-      "Created multiple APIs using Django REST Framework, for an online MOOC platform",
-      "Worked on development of  the front end of the platform using Angular",
-      "Used Angular, HTML, CSS, Bootstrap, Django, Postman, Git",
-    ],
-  },
+	{
+		label: "label3",
+		company: "Carl Zeiss India (Bangalore)",
+		role: "Full-Stack Developer Intern",
+		duration: "January 2022 - Present",
+		details: [],
+	},
+	{
+		label: "label2",
+		company: "Cartel101",
+		role: "Software Developer Intern",
+		duration: "April 2021 - May 2021",
+		details: [
+			"Worked on the front end of an Asset Performance Manager (APM) project",
+			"Performed multiple API integrations on the front end and used Redux for state management",
+			"Used React, Redux, HTML, CSS, Material-UI, Postman, Git, Figma",
+		],
+	},
+	{
+		label: "label1",
+		company: "ChaloPadhe",
+		role: "Software Developer Intern",
+		duration: "August 2020 - October 2020",
+		details: [
+			"Created multiple APIs using Django REST Framework, for an online MOOC platform",
+			"Worked on development of  the front end of the platform using Angular",
+			"Used Angular, HTML, CSS, Bootstrap, Django, Postman, Git",
+		],
+	},
 ];
 
 export const positionsOfResponsibility = [
-  "Core Member of The Society of Coders, IIIT-Naya Raipur",
-  "Website Manager of college’s website for Student Activity Center and IEEE Student Branch",
+	{
+		description: "Core Member of The Society of Coders, IIIT-Naya Raipur",
+		duration: "(September 2019 - March 2021)",
+	},
+	{
+		description:
+			"Website Manager of college’s website for Student Activity Center and IEEE Student Branch",
+		duration: "(September 2020 - July 2021)",
+	},
 ];
